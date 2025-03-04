6c9cae93aeca8ca4d51eac6e10790c4d8b24bc49
pranav-027 <pd2772001@gmail.com>
2025-03-04 16:15:41 +0530

    refactor part 2

--- my_app.js
+++ my_app.js
@@ -195,6 +195,7 @@ function getSeriesCompMessage(seriesDetails, compData) {
 
   return (
       `*Competition Series Announcement*\n\n` +
+      `\n\n` +
       `*${seriesDetails.name}*\n\n` +
       `${competitionList}\n\n` +
       `*Organizers:*\n${compData.compOrganizers}\n\n` +
